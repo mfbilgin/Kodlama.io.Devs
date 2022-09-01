b0a1292098f7c4685d53e5bf7dafee4698a538d0
Muhammet Fethullah Bilgin <mf.bilgin0@gmail.com>
2022-09-02 00:13:22 +0300

    Add project files.

---
+++ Core.Application/Pipelines/Authorization/AuthorizationBehavior.cs
@@ -0,0 +1,38 @@
+﻿using Core.Security.Extensions;
+using CoreCrossCuttingConcerns.Exceptions;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Application.Pipelines.Authorization
+{
+    public class AuthorizationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>, ISecuredResponse
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public AuthorizationBehavior(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            List<string>? roleClaims = _httpContextAccessor.HttpContext.User.ClaimRoles();
+            if (roleClaims == null) throw new AuthorizationException("Claim not found.");
+
+            bool isNotMatchedARoleClaimWithRequestRoles = roleClaims.FirstOrDefault(roleClaim => request.Roles.Any(role => role == roleClaim)).IsNullOrEmpty();
+            if (isNotMatchedARoleClaimWithRequestRoles) throw new AuthorizationException("You are not authorized");
+
+            TResponse response = await next();
+            return response;
+
+        }
+    }
+}
+ 
