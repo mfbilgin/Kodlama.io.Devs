283bf74b6508083e6be24f1056d95aaf2a92229a
Muhammet Fethullah Bilgin <mf.bilgin0@gmail.com>
2022-09-05 00:16:49 +0300

    Migration Added

---
+++ corePackages/Core.Persistence/Dynamic/Dynamic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.Dynamic
+{
+    public class Dynamic
+    {
+        public IEnumerable<Sort>? Sort { get; set; }
+        public Filter? Filter { get; set; }
+    }
+}
---
+++ corePackages/Core.Persistence/Repositories/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace Core.Persistence.Repositories
+{
+    public interface IRepository<T> : IQuery<T> where T : Entity
+    {
+        T Get(Expression<Func<T, bool>> predicate);
+    }
+}
